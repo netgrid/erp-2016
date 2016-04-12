884b5f47d12da06ec8badba38938bc3bfc0a000b
Roberto Indelicato <indelicatoroberto@hotmail.com>
2016-04-12 12:20:26 +0200

    no message

---
+++ model/src/main/java/it/netgrid/erp/model/RegitryGroup.java
@@ -0,0 +1,92 @@
+package it.netgrid.erp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="registry_group")
+public class RegitryGroup {
+	public static final String ID_FIELD_NAME = "reg_group_id";
+	public static final String NAME_ID_FIELD_NAME = "reg_group_registry_id";
+	public static final String REGISTRY_GROUP_PIVOT_FIELD_NAME = "reg_group_registry_pivot";
+	public static final String HIDDEN_FIELD_NAME = "reg_group_hidden";
+	
+	
+	@Id
+	@GeneratedValue
+	@Column(name=ID_FIELD_NAME)
+	private long id;
+	
+	@Column(name=NAME_ID_FIELD_NAME)
+	private long name;
+	
+	@Column(name=REGISTRY_GROUP_PIVOT_FIELD_NAME)
+	private long registry_group__pivot_id;
+	
+	@Column(name=HIDDEN_FIELD_NAME)
+	private boolean hidden;
+	
+	public RegitryGroup(){
+	
+	}
+	
+	
+	
+	public RegitryGroup(long id, long name, long registry_group_pivot_id, boolean hidden){
+		this.id=id;
+		this.name=name;
+		this.registry_group__pivot_id=registry_group_pivot_id;
+		this.hidden=hidden;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	public long getName() {
+		return name;
+	}
+
+
+
+	public void setName(long name) {
+		this.name = name;
+	}
+
+
+
+	public long getRegistry_group__pivot_id() {
+		return registry_group__pivot_id;
+	}
+
+
+
+	public void setRegistry_group__pivot_id(long registry_group__pivot_id) {
+		this.registry_group__pivot_id = registry_group__pivot_id;
+	}
+
+
+
+	public boolean isHidden() {
+		return hidden;
+	}
+
+
+
+	public void setHidden(boolean hidden) {
+		this.hidden = hidden;
+	}
+	
+	
+}
