e507bfdd6e3b862c61b48e518cc4cd645f0e1cfe
Michele Grigoli <grigolimichele@gmail.com>
2016-04-12 11:50:05 +0200

    sistemato orders

--- model/src/main/java/it/netgrid/erp/model/Order.java
+++ model/src/main/java/it/netgrid/erp/model/Order.java
@@ -2,7 +2,6 @@ package it.netgrid.erp.model;
 
 import java.util.Date; 
 
-
 @Entity(name="order")
 public class Order {
 	
@@ -11,11 +10,8 @@ public class Order {
 	public static final String CLIENT_FIELD_NAME = "ord_client";
 	public static final String PROVIDER_FIELD_NAME = "ord_client";
 	public static final String STATE_CODE_FIELD_NAME = "ord_state_code";
-	//public static final String DATE_ORDER_FIELD_NAME = "ord_date_order";
-	//public static final String DATE_SHIP_FIELD_NAME = "ord_date_ship";
 	public static final String DDT_FIELD_NAME = "ord_ddt";
 	public static final String INVOICE_FIELD_NAME = "ord_invoice";
-	
 	
 	
 	@Id
@@ -25,31 +21,82 @@ public class Order {
 	
 	@OneToMany
 	@JoinColumn(name = CLIENT_FIELD_NAME)
-//	private Client client;
+	private Client client;
 	
 	@OneToMany
 	@JoinColumn(name = PROVIDER_FIELD_NAME)
-//	private Provider provider;
+	private Provider provider;
 	
 	@Column(name = STATE_CODE_FIELD_NAME)
 	private String stateCode;
 	
-	/*
-	@Column(name = DATE_ORDER_FIELD_NAME)
-	private Date dateOrder;
-	
-	@Column(name = DATE_SHIP_FIELD_NAME)
-	private Date dateShip;
-	*/
 	@OneToOne
 	@JoinColumn(name = DDT_FIELD_NAME)
-//	private DDT ddt;
+	private DDT ddt;
 	
 	@OneToOne
 	@JoinColumn(name = INVOICE_FIELD_NAME)
-//	private Invoice invoice;
+	private Invoice invoice;
 	
 	public Order() {}
+
+	public Order(long id, Client client, Provider provider, String stateCode, DDT ddt, Invoice invoice) {
+		super();
+		this.id = id;
+		this.client = client;
+		this.provider = provider;
+		this.stateCode = stateCode;
+		this.ddt = ddt;
+		this.invoice = invoice;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Provider getProvider() {
+		return provider;
+	}
+
+	public void setProvider(Provider provider) {
+		this.provider = provider;
+	}
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public DDT getDdt() {
+		return ddt;
+	}
+
+	public void setDdt(DDT ddt) {
+		this.ddt = ddt;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
 
 
 
