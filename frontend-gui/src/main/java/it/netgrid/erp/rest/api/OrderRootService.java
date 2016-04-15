aee39f96f8d20e7cd0a7094256ff7e063f332b0c
Alberto Harka <alberto_harka@yahoo.com>
2016-04-15 12:02:00 +0200

    refractor di OrderService in OrderRootService

--- frontend-gui/src/main/java/it/netgrid/erp/rest/api/OrderRootService.java
+++ frontend-gui/src/main/java/it/netgrid/erp/rest/api/OrderRootService.java
@@ -2,20 +2,33 @@ package it.netgrid.erp.rest.api;
 
 import java.sql.SQLException;
 
+
+
+import org.apache.commons.lang3.Validate;
+
+import com.google.inject.Inject;
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 
 import it.netgrid.erp.model.Order;
+import it.netgrid.erp.model.OrderItem;
 
-public class OrderService extends TemplateCrudService<Order, Long> {
+public class OrderRootService extends TemplateCrudService<Order, Long> {
 
-	protected OrderService(ConnectionSource connection) {
+	private Dao<Order, Long> orderDao;
+	private Dao<OrderItem, Long> itemDao;
+	@Inject
+	public OrderRootService(ConnectionSource connection, Dao<Order, Long> orderDao, Dao<OrderItem, Long> itemDao) {
 		super(connection);
+		this.orderDao=orderDao;
+		this.itemDao=itemDao;
 	}
 
 	@Override
 	public int createRaw(Order order) throws SQLException, IllegalArgumentException {
-		
-		return 0;
+		int result=0;
+
+		return result;
 	}
 
 	@Override
