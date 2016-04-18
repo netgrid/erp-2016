54bc015e68b40e4137336ba3d2eaec1020e542c9
Enrico <enricoantonello96@gmail.com>
2016-04-18 09:56:49 +0200

    inserimento unità di misura

--- model/src/main/java/it/netgrid/erp/model/Product.java
+++ model/src/main/java/it/netgrid/erp/model/Product.java
@@ -1,5 +1,7 @@
 package it.netgrid.erp.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ public class Product {
 	public static final String PRODUCT_PRICE="pro_product_price";
 	public static final String PRODUCT_DISCOUNT="pro_product_discount";
 	public static final String PRODUCT_QUANTITY="pro_product_quatity";
+	public static final String UNITS_OF_MEASURE="pro_units_of_measure";
 	
 	@Id
 	@GeneratedValue
@@ -22,20 +25,23 @@ public class Product {
 	private String product;
 	
 	@Column(name=PRODUCT_PRICE)
-	private int price;
+	private BigDecimal price;
 	
 	@Column(name=PRODUCT_QUANTITY)
-	private int quantity;
+	private BigDecimal quantity;
+	
+	@Column(name=UNITS_OF_MEASURE)
+	private String units_of_measure;
 	
 	public Product(){	
 	}
 	
-	public Product(Long id,String product,int price,int quantity){
+	public Product(Long id,String product,BigDecimal price,BigDecimal quantity, String misure){
 		this.id = id;
 		this.product = product;
 		this.price = price;
-		
 		this.quantity = quantity;
+		this.units_of_measure = misure;
 		
 	}
 	
@@ -55,19 +61,27 @@ public class Product {
 		this.product = product;
 	}
 	
-	public int getPrice() {
+	public BigDecimal getPrice() {
 		return price;
 	}
 
-	public void setPrice(int price) {
+	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
 	
-	public int getQuantity() {
+	public BigDecimal getQuantity() {
 		return quantity;
 	}
 
-	public void setQuantity(int quantity) {
+	public void setQuantity(BigDecimal quantity) {
 		this.quantity = quantity;
+	}
+	
+	public String getUnitOfMeasure() {
+		return units_of_measure;
+	}
+
+	public void setUnitOfMeasure(String units_of_measure) {
+		this.units_of_measure = units_of_measure;
 	}
 }
