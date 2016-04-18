8b391205af5f9a333434bd901eb8840913a44f4b
Enrico <enricoantonello96@gmail.com>
2016-04-18 12:57:09 +0200

    modifiche createRaw

--- frontend-gui/src/main/java/it/netgrid/erp/rest/api/ProductCrudService.java
+++ frontend-gui/src/main/java/it/netgrid/erp/rest/api/ProductCrudService.java
@@ -2,6 +2,10 @@ package it.netgrid.erp.rest.api;
 
 import java.sql.SQLException;
 
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.validator.Validator;
+import org.apache.commons.validator.ValidatorResources;
+
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.sun.xml.bind.v2.model.core.ID;
@@ -12,35 +16,19 @@ import it.netgrid.erp.model.Product;
 public class ProductCrudService extends TemplateCrudService<Product, Long>{
 	
 	private Dao<Product, Long> dao;
+	
+	Validate validator = new Validate();
 
 	protected ProductCrudService(ConnectionSource connection, Dao<Product, Long> dao) {
 		super(connection);
 		this.dao = dao;
 	}
-
+	 
 	@Override
 	public int createRaw(Product object) throws SQLException, IllegalArgumentException {
-			if(object.getPrice() != null){
-				if(object.getProduct() != null){
-					if(object.getQuantity() != null){
-						if(object.getUnitOfMeasure() != null){
-							return dao.create(object);
-						}	
-						else{
-							return 1;
-						}
-					}
-					else{
-						return 1;
-					}
-				}
-				else{
-					return 1;
-				}
-			}
-			else{
-				return 1;
-			}
+			validator.notNull(object.getUnitOfMeasure());
+			validator.notNull(object.getProduct());
+			return dao.create(object);
 	}
 
 	@Override
