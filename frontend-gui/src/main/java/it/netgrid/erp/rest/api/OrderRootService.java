70e54691cb9390f43837c1b637d42807b5d001f5
Alberto Harka <alberto_harka@yahoo.com>
2016-04-18 11:45:35 +0200

    Completato la funzione createRow(OrderRoot);

--- frontend-gui/src/main/java/it/netgrid/erp/rest/api/OrderRootService.java
+++ frontend-gui/src/main/java/it/netgrid/erp/rest/api/OrderRootService.java
@@ -1,11 +1,7 @@
 package it.netgrid.erp.rest.api;
 
 import java.sql.SQLException;
-
-
-
 import org.apache.commons.lang3.Validate;
-
 import com.google.inject.Inject;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
@@ -16,6 +12,10 @@ import it.netgrid.erp.model.dto.OrderRoot;
 
 public class OrderRootService extends TemplateCrudService<OrderRoot, Long> {
 
+	private static final String INVALID_ORDER = "order";
+	private static final String INVALID_ORDER_ITEMS = "orderitems";
+	private static final String EMPTY_ORDER_ITEMS = "empty orderitems";
+	
 	private Dao<Order, Long> orderDao;
 	private Dao<OrderItem, Long> itemDao;
 	
@@ -29,7 +29,17 @@ public class OrderRootService extends TemplateCrudService<OrderRoot, Long> {
 	@Override
 	public int createRaw(OrderRoot order) throws SQLException, IllegalArgumentException {
 		int result=0;
-
+		//controllo validità
+		Validate.notNull(order.getOrder(), INVALID_ORDER);
+		Validate.notNull(order.getOrderItems(), INVALID_ORDER_ITEMS);
+		Validate.notEmpty(order.getOrderItems(), EMPTY_ORDER_ITEMS);
+		
+		//inserimento ordine
+		result += this.orderDao.create(order.getOrder());
+		for(OrderItem item : order.getOrderItems()){
+			item.setOrder(order.getOrder());
+			result += this.itemDao.create(item);
+		}
 		return result;
 	}
 
